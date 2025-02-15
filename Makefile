d510028b6aaaca683ef2daa202a5b53d88761577
Eduardo Victor <eduvictornobrega@gmail.com>
2025-02-14 22:17:58 -0300

    chore: initial config

---
+++ Makefile
@@ -0,0 +1,36 @@
+# install-pyenv:
+# 	@command -v pyenv >/dev/null 2>&1 || curl https://pyenv.run | bash
+
+# setup-project:install-pyenv
+# 	pyenv versions | grep 3.12.0 || pyenv install 3.12.0
+# 	pyenv local 3.12.0
+# 	python --version
+# 	python -m venv venv
+# 	. venv/bin/activate
+# 	pip install --upgrade pip
+# 	pip install poetry
+# 	poetry install --only dev --no-root
+
+install:
+	. venv/bin/activate
+	poetry lock --no-update
+	poetry install
+
+migrations:
+	poetry run alembic revision --autogenerate
+
+migrate:
+	poetry run alembic upgrade head
+
+seed:
+	docker compose --env-file=.env -f docker/docker-compose.yaml exec bank-api python scripts/seeds.py
+
+pre-commit:
+	@poetry run pre-commit run --all-files
+
+tests:
+	@echo "Running tests"
+	@poetry run pytest --disable-warnings -vv --cov-report=html --cov .
+
+up:
+	docker compose --env-file=.env -f docker/docker-compose.yaml up --build -d
---
+++ docker/entrypoint.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+# This file is used to run the application inside the container on startup development server.
+echo "Instalando dependências"
+cd /home/app
+make install
+
+echo "Esperando o banco de dados conectar"
+postgres_ready() {
+poetry run python3 << END
+import os
+import sys
+import asyncio
+import asyncpg
+
+db_url = os.getenv('DATABASE_URL', 'postgresql+asyncpg://postgres:postgres@bank-api-postgres:5432/bank-api')
+print(db_url)
+
+async def connect():
+    try:
+        await asyncpg.connect(db_url.replace("+asyncpg", ""))
+        print("Conectado ao banco de dados")
+        sys.exit(0)
+    except Exception as e:
+        print(e)
+        sys.exit(-1)
+
+loop = asyncio.new_event_loop()
+loop.run_until_complete(connect())
+END
+}
+
+until postgres_ready; do
+  >&2 echo "PostgreSQL não está disponível ainda - Espere..."
+  sleep 1
+done
+
+echo "Rodando migrações"
+make migrate
+export ENVIRONMENT=${ENVIRONMENT:-development}
+export PORT=${APP_PORT:-8000}
+export PYTHONDONTWRITEBYTECODE=1
+export PYTHONPATH=/home/app
+
+python3 -m poetry run debugpy --listen 0.0.0.0:5676 -m gunicorn "src.main:create_app()" \
+    --worker-class uvicorn_workers.RestartableUvicornWorker \
+    --bind 0.0.0.0:$PORT \
+    --graceful-timeout 0 \
+    --timeout 0 \
+    --access-logfile - \
+    --chdir $PYTHONPATH \
+    --reload
